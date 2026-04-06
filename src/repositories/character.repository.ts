7b8f789d95c78d691e9cdc160fd96ba3b2cde66f
Turas <182771626+Turas39@users.noreply.github.com>
2026-04-06 23:50:16 +0200

    add repositories/character.repository.ts

---
+++ src/repositories/character.repository.ts
@@ -0,0 +1,19 @@
+import { CharacterModel } from "../models/Character";
+
+export class CharacterRepository {
+    create(data: any) {
+        return CharacterModel.create(data);
+    }
+
+    findAll() {
+        return CharacterModel.find().populate("bestFriend");
+    }
+
+    findById(id: string) {
+        return CharacterModel.findById(id).populate("bestFriend");
+    }
+
+    delete(id: string) {
+        return CharacterModel.findByIdAndDelete(id)
+    }
+}
